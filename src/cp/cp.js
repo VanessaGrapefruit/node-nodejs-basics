5c957ec5d12230e8931290347cf120ce3363d21c
Aliaksei Rachkouski <16lexa1602@gmail.com>
2022-12-06 00:32:59 +0200

    feat: continue task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,16 @@
+import { fork } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+	const cp = fork('./files/script.js', args, { stdio: 'pipe' });
+
+	process.stdin.on('data', (data) => {
+		process.stdout.write(`send to child process: ${data.toString()}`);
+		cp.stdin.write(data);
+	});
+	cp.stdout.on('data', (data) => {
+		process.stdout.write(`recieve from child process: ${data.toString()}`);
+		process.stdout.write(data);
+	});
 };
 
 spawnChildProcess();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,7 +1,14 @@
+import { workerData, parentPort } from 'worker_threads';
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
+	try {
+		parentPort.postMessage({ status: 'resolved', data: nthFibonacci(workerData) });
+	} catch {
+		parentPort.postMessage({ status: 'error', data: null });
+	}
     // This function sends result of nthFibonacci computations to main thread
 };
 
